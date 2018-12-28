5ec4e23ef92ce58a6a56c2d99e0713ffb2c913a6
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2018-12-28 10:15:05 -0500

    Add two more fuzz decode corpora

---
+++ fuzz/decode/c/setup.sh
@@ -0,0 +1,3 @@
+# Source this file
+export USE_QPACK_05=1
+ARGS='interop-decode -i @@ -o /dev/null -s 100 -t 256'
---
+++ fuzz/decode/d/setup.sh
@@ -0,0 +1,3 @@
+# Source this file
+export USE_QPACK_05=0
+ARGS='fuzz-decode -i fuzz/decode/d/preamble -f @@ -s 100 -t 256'
