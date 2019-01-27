a79741c2f5b5898bb650368108ac790415aa82ca
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2019-01-27 07:55:50 -0500

    Rewrite run-qif script in Perl for portability

---
+++ test/run-qif.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+#
+# Script to run a QIF file.
+
+use strict;
+use warnings;
+
+use File::Compare qw(compare);
+use File::Copy qw(copy);
+use File::Path qw(remove_tree);
+use File::Spec::Functions qw(catfile);
+use Getopt::Long qw(GetOptions);
+
+my $cleanup = 1;
+
+GetOptions(
+    "aggressive=i"      => \my $aggressive,
+    "table-size=i"      => \my $table_size,
+    "immed-ack=i"       => \my $immed_ack,
+    "risked-streams=i"  => \my $risked_streams,
+    "no-cleanup"        => sub { $cleanup = 0 },
+);
+
+my $dir = catfile(($ENV{TMP} || $ENV{TEMP} || "/tmp"),
+                                                "run-qif-out-" . rand . $$);
+mkdir $dir or die "cannot create temp directory $dir";
+if (!$cleanup)
+{
+    print "created temp directory: $dir\n";
+}
+
+END {
+    if (defined($dir) && $cleanup) {
+        remove_tree($dir);
+    }
+}
+
+my $encode_log = catfile($dir, "encode.log");
+my $qif_file = catfile($dir, "qif");
+my $bin_file = catfile($dir, "out");
+my $resulting_qif_file = catfile($dir, "qif-result");
+
+my ($encode_args, $decode_args) = ('', '');
+if ($aggressive) {
+    $encode_args="$encode_args -A";
+}
+
+if ($immed_ack) {
+    $encode_args="$encode_args -a 1";
+}
+
+if (defined $risked_streams) {
+    $encode_args = "$encode_args -s $risked_streams";
+    $decode_args = "$decode_args -s $risked_streams";
+}
+
+if (defined $table_size) {
+    $encode_args = "$encode_args -t $table_size";
+    $decode_args = "$decode_args -t $table_size";
+}
+
+copy($ARGV[0], $qif_file) or die "cannot copy original $ARGV[0] to $qif_file";
+
+system("interop-encode $encode_args -i $qif_file -o $bin_file 2>$encode_log")
+    and die "interop-encode failed";
+system("interop-decode $decode_args -i $bin_file -o $resulting_qif_file")
+    and die "interop-decode failed";
+system("sort-qif.pl --strip $qif_file > $qif_file.canonical")
+    and die "sort-qif.pl $qif_file failed";
+system("sort-qif.pl --strip $resulting_qif_file>$resulting_qif_file.canonical")
+    and die "sort-qif.pl $qif_file failed";
+
+exit compare "$qif_file.canonical", "$resulting_qif_file.canonical";
