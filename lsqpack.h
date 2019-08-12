eb44e36071668e391429b7fe510e9aa7d4bf30af
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2019-08-12 16:19:25 -0400

    Update version to 0.9.10

--- lsqpack.h
+++ lsqpack.h
@@ -48,7 +48,7 @@ typedef SSIZE_T ssize_t;
  */
 #define LSQPACK_MAJOR_VERSION 0
 #define LSQPACK_MINOR_VERSION 9
-#define LSQPACK_PATCH_VERSION 9
+#define LSQPACK_PATCH_VERSION 10
 
 /** Let's start with four billion for now */
 typedef unsigned lsqpack_abs_id_t;
