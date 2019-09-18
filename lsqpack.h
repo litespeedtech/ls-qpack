6e2bdf46a214efa0f934d931f29058238ed32f46
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2019-09-18 11:02:35 -0400

    Update version to 0.10.0
    There are no code changes since 0.9.14.  This is simply done to
    indicate that this library supports the latest QPACK Internet
    Draft:
    
      https://tools.ietf.org/html/draft-ietf-quic-qpack-10

--- lsqpack.h
+++ lsqpack.h
@@ -47,8 +47,8 @@ typedef SSIZE_T ssize_t;
  * qpack-11 (if such draft ever published) will correspond to version 0.11.
  */
 #define LSQPACK_MAJOR_VERSION 0
-#define LSQPACK_MINOR_VERSION 9
-#define LSQPACK_PATCH_VERSION 14
+#define LSQPACK_MINOR_VERSION 10
+#define LSQPACK_PATCH_VERSION 0
 
 /** Let's start with four billion for now */
 typedef unsigned lsqpack_abs_id_t;
