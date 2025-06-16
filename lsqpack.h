1e9c5b8e59f8161c54f168a570c8bfdc59ded0c3
George Wang <gwang@litespeedtech.com>
2025-06-16 18:02:38 -0400

    [VERSION] update version to 2.6.2

--- lsqpack.h
+++ lsqpack.h
@@ -44,7 +44,7 @@ extern "C" {
 
 #define LSQPACK_MAJOR_VERSION 2
 #define LSQPACK_MINOR_VERSION 6
-#define LSQPACK_PATCH_VERSION 1
+#define LSQPACK_PATCH_VERSION 2
 
 /** Let's start with four billion for now */
 typedef unsigned lsqpack_abs_id_t;
