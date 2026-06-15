cbea62cad848b72e7696aa1a982f4a7a8c9c7806
Dmitri Tikhonov <dtikhonov@live.com>
2026-06-15 18:19:08 -0400

    [VERSION] Bump to 2.6.3; update Copyright notices

--- lsqpack.h
+++ lsqpack.h
@@ -5,7 +5,7 @@
 /*
 MIT License
 
-Copyright (c) 2018 - 2023 LiteSpeed Technologies Inc
+Copyright (c) 2018 - 2026 LiteSpeed Technologies Inc
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -44,7 +44,7 @@ extern "C" {
 
 #define LSQPACK_MAJOR_VERSION 2
 #define LSQPACK_MINOR_VERSION 6
-#define LSQPACK_PATCH_VERSION 2
+#define LSQPACK_PATCH_VERSION 3
 
 /** Let's start with four billion for now */
 typedef unsigned lsqpack_abs_id_t;
